ef9f354e36b787a59111d42e7936f20f6310f92b
Dhara <dhara.vakhare@gmail.com>
2023-06-12 17:34:58 +0530

    nested React Element

--- App.js
+++ App.js
@@ -1,7 +1,12 @@
 //  using React render element
 
-//root.render replace not append inside root element and passdata from root.render method 
-const heading = React.createElement("h1", {id:"heading"}, "Hello World React Element");
-console.log(heading)
-const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(heading);//use root.render
+//root.render replace not append inside root element and passdata from root.render method
+const heading = React.createElement
+        ("div", { id: "parent" },[
+            React.createElement("div", { id: "child" },[
+            React.createElement("h3", { id: "heading1" },"heading1"),
+            React.createElement("h4", { id: "heading2" },"heading2")])
+        ]);
+console.log(heading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading); //use root.render

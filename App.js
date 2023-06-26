1bf61bb51d08b1ef04208f1c0ad0a5bcd7667dd0
Dhara <dhara.vakhare@gmail.com>
2023-06-26 23:33:10 +0530

    react element, jsx, functional , react fragment

--- App.js
+++ App.js
@@ -1,14 +1,41 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-//  using React render element
 
-//root.render replace not append inside root element and passdata from root.render method
-const heading = React.createElement
-        ("div", { id: "parent" },[
-            React.createElement("div", { id: "child" },[
-            React.createElement("h3", { id: "heading1" },"heading1"),
-            React.createElement("h4", { id: "heading2" },"heading2")])
-        ]);
-console.log(heading);
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading); //use root.render
+// Nested header element using React.createElement
+
+// React.createElement = javascript object = render into html
+const heading = React.createElement("div", { class: "title" },[
+                React.createElement("h1", {},"h1"),
+                React.createElement("h1", {},"h2"),
+                React.createElement("h1", {},"h3")]);
+
+
+// Nested header element using JSX
+// jsx = React.createElement = javascript - Object = render into html
+// jsx transpile code into  React.createElement which jsEngine can understand 
+const jsxheading = (<div className="title">
+        <h1>Heading 1</h1>
+        <h2>Heading 2</h2>
+        <h3>Heading 3</h3>
+    </div>);
+
+
+// function component
+const TitleComponent = () =>{
+    return <h2>Component Composition</h2>
+}
+const Headingcomponent = () =>{
+    return <div className="container">
+        <h1>Heading 1</h1> 
+        {TitleComponent()}
+        {<TitleComponent/>}
+
+        {<TitleComponent></TitleComponent>}
+
+    </div>
+};
+
+
+const root= ReactDOM.createRoot(document.getElementById("root"))
+//root.render(jsxheading); render react element
+root.render(<Headingcomponent/>);  //render functional component
